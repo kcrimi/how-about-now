8ce681af126687587bd3a91d6d13ef809ed4fcbf
kcrimi <kevincrimi@gmail.com>
2018-01-31 20:26:13 -0500

    Correctly logging closed ad

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+const rp = require('request-promise')
  
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -13,6 +14,21 @@ app.get('/awake', (req, res) => {
 	res.send('I\'m awake!')
 })
 
-setInterval(function() {
+// Check if Northside Festival submission page is up
+setInterval(() => {
+	rp({uri:"https://www.sonicbids.com/find-gigs/northside-festival-2018/"})
+	// rp({uri:"https://www.google.com/"})
+	.then(((response) => {
+		// Email me
+		return
+	}))
+	.catch ((error) => {
+		if (error.statusCode == 403) {
+			console.log("Ad not open as of "+new Date())
+		}
+	})
+}, 3000)//21600000) // every 6 hours
+
+setInterval(() => {
     rp({uri:"https://how-about-now.herokuapp.com/awake"})
 }, 600000) // every 10 minutes (600000)
