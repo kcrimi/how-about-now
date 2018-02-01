59b6a25edd7f662cb7fba4dfa679f430eefd8831
kcrimi <kevincrimi@gmail.com>
2018-01-31 20:08:20 -0500

    Heroku awake call

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.get('/awake', (req, res) => {
+	res.send('I\'m awake!')
+})
+
+setInterval(function() {
+    rp({uri:"https://how-about-now.herokuapp.com/awake"})
+}, 600000) // every 10 minutes (600000)
